0092df2da5bfc12245a305b98343473200f904a1
Levchenko Dmitry <levchenkodm13@gmail.com>
2021-04-15 22:06:37 +0300

    add migrate

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(ArticlesSeeder::class);
+        $this->call(CategoriesSeeder::class);
+        $this->call(FiltersSeeder::class);
+        $this->call(MenusSeeder::class);
+        $this->call(PortfoliosSeeder::class);
+        $this->call(SlidersSeeder::class);
     }
 }
