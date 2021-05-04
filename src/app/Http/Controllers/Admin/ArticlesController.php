8e35fc9606f335e27c763807b87c0bd0dea3010a
Levchenko Dmitry <levchenkodm13@gmail.com>
2021-05-04 10:06:55 +0300

    request

--- src/app/Http/Controllers/Admin/ArticlesController.php
+++ src/app/Http/Controllers/Admin/ArticlesController.php
@@ -5,12 +5,15 @@ namespace Corp\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 
 use Corp\Http\Requests;
+use Corp\Http\Requests\ArticleRequest;
 use Corp\Http\Controllers\Controller;
 
 use Corp\Repositories\ArticlesRepository;
 use Gate;
 
 use Corp\Category;
+
+
 
 class ArticlesController extends AdminController
 {
@@ -91,9 +94,15 @@ class ArticlesController extends AdminController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ArticleRequest $request)
     {
         //
+        $result = $this->a_rep->addArticle($request);
+
+        if(is_array($result) && !empty($result['error'])){
+            return back()->with($result);
+        }
+        return redirect('/admin')->with($result);
     }
 
     /**
